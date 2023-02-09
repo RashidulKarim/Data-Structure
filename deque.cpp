ec1f57d1a51a9cfb7a10131402007cc3a22aa415
rashidul-xs <rashidul@xpeedstudio.com>
2023-02-09 20:27:46 +0600

    Implement deque with doubly linkList

---
+++ deque.cpp
@@ -0,0 +1,241 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+template<class T>
+class Node
+{
+public:
+    T data;
+    Node<T>* next;
+    Node<T>* prev;
+};
+
+template<class T>
+class Deque
+{
+public:
+    Node<T>* head;
+    Node<T>* tail;
+    int sz, isReverse;
+
+    Deque()
+    {
+        head = NULL;
+        tail = NULL;
+        sz = 0;
+        isReverse = 0;
+    }
+
+    void reverse()
+    {
+        if(isReverse)
+            isReverse = 0;
+        else
+            isReverse = 1;
+    }
+
+    Node<T>* createNode(T data)
+    {
+        Node<T>* newNode = new Node<T>;
+        newNode->data = data;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+        return newNode;
+    }
+
+    void pushAtHead(T data)
+    {
+        Node<T>* newNode = createNode(data);
+        if(sz == 0)
+        {
+            head = newNode;
+            tail = newNode;
+            sz++;
+            return;
+        }
+        head->prev = newNode;
+        newNode->next = head;
+        head = newNode;
+        sz++;
+        return;
+    }
+
+    void pushAtBack(T data)
+    {
+        Node<T> * newNode = createNode(data);
+        if(sz == 0)
+        {
+            head = newNode;
+            tail = newNode;
+            sz++;
+            return;
+        }
+        newNode->prev =tail;
+        tail->next = newNode;
+        tail = newNode;
+        sz++;
+        return;
+    }
+
+    void push_front(T data)
+    {
+        if(isReverse)
+            pushAtBack(data);
+        else
+            pushAtHead(data);
+    }
+
+    void push_back(T data)
+    {
+        if(isReverse)
+            pushAtHead(data);
+        else
+            pushAtBack(data);
+    }
+
+    void removeFromBack()
+    {
+        if(sz == 0)
+        {
+            cout<<"Deque is empty\n";
+            return;
+        }
+
+        if(sz == 1)
+        {
+            delete head;
+            head = NULL;
+            tail = NULL;
+            sz--;
+            return;
+        }
+
+        Node<T>* lastNode = tail;
+        tail = tail->prev;
+        tail->next = NULL;
+        delete lastNode;
+        sz--;
+    }
+
+    void removeFromHead()
+    {
+        if(sz == 0)
+        {
+            cout<<"Deque is empty\n";
+            return;
+        }
+
+        if(sz == 1)
+        {
+            delete head;
+            head = NULL;
+            tail = NULL;
+            sz--;
+            return;
+        }
+        Node<T>* firstNode = head;
+        head = head->next;
+        head->prev = NULL;
+        sz--;
+        delete firstNode;
+    }
+
+    void pop_back()
+    {
+        if(isReverse)
+            removeFromHead();
+        else
+            removeFromBack();
+    }
+
+    void pop_front()
+    {
+        if(isReverse)
+            removeFromBack();
+        else
+            removeFromHead();
+    }
+
+    T front()
+    {
+        if(sz == 0)
+        {
+            cout<<"Deque is empty\n";
+            return -1;
+        }
+
+        if(isReverse)
+            return tail->data;
+        else
+            return head->data;
+    }
+
+    T back()
+    {
+        if(sz == 0)
+        {
+            cout<<"Deque is empty\n";
+            return -1;
+        }
+        if(isReverse)
+            return head->data;
+        else
+            return tail->data;
+    }
+    int size()
+    {
+        return sz;
+    }
+
+    T max()
+    {
+        Node<T>* temp = head->next;
+        if(sz == 0)
+            return -1;
+        T maxx = head->data;
+
+        while(temp != NULL)
+        {
+            if(temp->data > maxx)
+                maxx = temp->data;
+            temp = temp->next;
+        }
+        return maxx;
+    }
+
+    T min()
+    {
+        Node<T>* temp = head->next;
+        if(sz == 0)
+            return -1;
+        T min = head->data;
+
+        while(temp != NULL)
+        {
+            if(temp->data < min)
+                min = temp->data;
+            temp = temp->next;
+        }
+        return min;
+    }
+};
+
+
+int main()
+{
+    Deque<double> dq;
+    dq.push_front(10.10);
+    dq.push_front(5);
+    dq.push_back(20);
+    dq.push_back(30);
+    dq.push_back(40);
+    dq.pop_back();
+    dq.pop_front();
+    dq.push_back(50.50);
+    dq.push_front(2.2);
+    dq.reverse();
+    cout<<"First element "<<dq.front()<<" last element "<<dq.back()<<" size is "<<dq.size()<<" Max:"<<dq.max()<<" Min:" <<dq.min();
+
+    return 0;
+}
